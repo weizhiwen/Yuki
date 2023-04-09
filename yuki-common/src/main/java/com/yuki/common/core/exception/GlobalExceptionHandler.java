582f75746b6b28c03704e1e4f5dd66d417b4435f
shixin <763107756@qq.com>
2023-04-09 12:03:13 +0800

    remove unused import

--- yuki-common/src/main/java/com/yuki/common/core/exception/GlobalExceptionHandler.java
+++ yuki-common/src/main/java/com/yuki/common/core/exception/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
