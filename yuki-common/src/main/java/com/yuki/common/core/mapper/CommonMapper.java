54fde2fcc7385408031956af4ceb25451ecf1495
shixin <763107756@qq.com>
2023-05-31 19:22:13 +0800

    rename

--- yuki-common/src/main/java/com/yuki/common/core/mapper/CommonMapper.java
+++ yuki-common/src/main/java/com/yuki/common/core/mapper/CommonMapper.java
@@ -3,7 +3,7 @@ package com.yuki.common.core.mapper;
 import org.mapstruct.MappingTarget;
 import org.springframework.core.convert.converter.Converter;
 
-public abstract class BaseMapper<S, T> implements Converter<S, T> {
+public interface CommonMapper<S, T> extends Converter<S, T> {
     public abstract T map(S s);
 
     public abstract S reviseMap(T t);
