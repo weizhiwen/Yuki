88d184fee98978f17ae7391fb644debd19ea9472
shixin <763107756@qq.com>
2023-08-20 17:52:17 +0800

    fix 删除多个

--- yuki-framework/src/main/java/com/yuki/framework/dict/web/DictTypeController.java
+++ yuki-framework/src/main/java/com/yuki/framework/dict/web/DictTypeController.java
@@ -13,6 +13,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/dict-types")
 @RequiredArgsConstructor
@@ -68,7 +70,7 @@ public class DictTypeController extends BaseBusinessController {
 
     @Override
     @DeleteMapping("/{ids}")
-    public JsonResult<String> delete(@PathVariable Long ids) {
+    public JsonResult<String> delete(@PathVariable Long[] ids) {
         return super.delete(ids);
     }
 }
