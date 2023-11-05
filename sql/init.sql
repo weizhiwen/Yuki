3f2df701aa19889c108903a5e1e46a61be377b6f
shixin <weizhiwen23@gmail.com>
2023-11-05 16:52:56 +0800

    修改init SQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,3 @@
 create database if not exists yuki;
-insert into user(user_no, username, tel, password, is_enabled)
-values ('yuki', 'yuki', '12345678910' '$2a$10$0i.tpAuoMzsdyGWYjv3HCOfnLKp4f6LRmLTpoE.S0FPCdAMbrJ/Vm', true);
+insert into yuki.user(username, name, tel, password, is_enabled)
+values ('yuki', 'yuki', '12345678910' '$2a$10$0i.tpAuoMzsdyGWYjv3HCOfnLKp4f6LRmLTpoE.S0FPCdAMbrJ/Vm', 1);
